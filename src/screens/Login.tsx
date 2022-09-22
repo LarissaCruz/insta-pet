1b9ec5383dc40e37cb9cff9fe7be3bd3c05c8fa8
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-09-22 07:31:37 -0300

    adding login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -7,22 +7,51 @@ import { colors } from "../components/colors";
 import Card from "../components/Card/Card";
 import { FlatList } from "react-native";
 import SmallText from "../components/Texts/SmallText";
+import BigText from "../components/Texts/BigText";
 import Avatar from "../components/Avatar/Avatar";
 import Button from "../components/Button/Button";
 import { AlignCenter, SpaceBetween } from "../components/shared";
 import RegularText from "../components/Texts/RegularText";
 import { MaterialIcons } from "@expo/vector-icons";
+import imageWelcome from "../assets/image/imageWelcome.png";
+import Input from "../components/Inputs/Input";
+import { AntDesign } from "@expo/vector-icons";
 
 const HomeContainer = styled(Container)`
   background-color: ${colors.white};
-  width: 100%;
+  padding: 16px;
+`;
+const ImageComponent = styled.Image`
+  width: 194;
+  heigth: 194;
 `;
 const Login: FunctionComponent = () => {
   return (
     <HomeContainer>
-      <RegularText textStyles={{ fontSize: 24 }}>Perfil</RegularText>
-      <SmallText textStyles={{ color: "white", fontSize: 12 }}>
-     
+      <AlignCenter>
+        <BigText>Instapet</BigText>
+      </AlignCenter>
+      <AlignCenter>
+        <ImageComponent source={imageWelcome} />
+        <SmallText>A rede social do seu bichinho</SmallText>
+      </AlignCenter>
+      <Input label={"Email"} placeholder={"adalovelace@email.com"} />
+      <Input label={"Senha"} placeholder={"*******"} />
+      <Button styles={{ marginTop: 20 }}>
+        <AntDesign name="check" size={24} color="white" />
+        <SmallText
+          textStyles={{
+            color: "white",
+            fontWeight: "bold",
+            marginHorizontal: 4,
+          }}
+        >
+          Entrar
+        </SmallText>
+      </Button>
+      <Button styles={{ marginTop: 20, backgroundColor: "white" }}>
+        <SmallText textStyles={{ fontWeight: "bold" }}>Cadastrar</SmallText>
+      </Button>
     </HomeContainer>
   );
 };
