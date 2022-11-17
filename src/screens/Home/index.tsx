d7e9aed9e4f187665cdfc862041951e2bd4f2095
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-11-17 18:02:57 -0300

    Feat: API service

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,20 @@ import { StatusBar } from "expo-status-bar";
 import { FlatList } from "react-native";
 import {HomeContainer} from './styles'
 import Card from "../../components/Card/Card";
-
+import api from "../../services/Api";
 
 
 const Home: FunctionComponent = () => {
+  React.useEffect(() => {
+     const requestPost = async () => {
+      const response = await api.get("publi");
+      //atualiza o response quando muda a variável feed
+      console.log(response.data.publis)
+     }
+     requestPost();
+  }, []);
+  
+
   return (
     <HomeContainer>
       <StatusBar />
@@ -28,3 +38,7 @@ const Home: FunctionComponent = () => {
 };
 
 export default Home;
+function requestPost() {
+  throw new Error("Function not implemented.");
+}
+
